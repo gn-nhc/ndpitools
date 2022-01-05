4e4a760d0bc4c9ad5241c31af4397b7a319b1d7b
akm <akm2000@gmail.com>
2022-01-05 13:54:29 +0900

    $ ls . | grep -v .git | grep -v .gitignore | xargs rm -rf && mkdir -p tmp && cd tmp && curl -L -o ndpitools-1.5-3.tar.bz2 https://www.imnc.in2p3.fr/pagesperso/deroulers/software/ndpitools/download/ndpitools-1.5/ndpitools-1.5-3.tar.bz2 && tar jxf ndpitools-1.5-3.tar.bz2 && cp -rf ndpitools-1.5-3/* .. && cd .. && git add .

--- build_ndpitools.sh
+++ build_ndpitools.sh
@@ -2,9 +2,9 @@
 
 USE_LIBJPEGTURBO=yes
 
-LIBJPEGTURBOVERSION=1.4.1
-JPEGVERSION=9a
-TIFFVERSION=4.0.6
+LIBJPEGTURBOVERSION=2.1.2
+JPEGVERSION=9d
+TIFFVERSION=4.3.0
 
 BINARIES="ndpi2tiff ndpisplit ndpisplit-s ndpisplit-m ndpisplit-mJ ndpisplit-s-m ndpisplit-s-mJ"
 
@@ -13,30 +13,38 @@ BASEDIR=$PWD
 if [ x${USE_LIBJPEGTURBO} = xyes ] ; then
   ln -s libjpeg-turbo-${LIBJPEGTURBOVERSION} jpeg
   cd jpeg
-  autoreconf -fiv
+  cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=$BASEDIR -DENABLE_SHARED=0 .
+  make
+  make install
   cd ..
-else
-  ln -s jpeg-${JPEGVERSION} jpeg
 fi
-cd jpeg
+
+if [ x${USE_LIBJPEGTURBO} != xyes -o ! -e lib/libjpeg.a ] ; then
+  rm -f jpeg
+  ln -s jpeg-${JPEGVERSION} jpeg
+  cd jpeg
   ./configure --prefix=$BASEDIR
   make
   make install
+  cd ..
+fi
+
 cd $BASEDIR
 
 JPEGDIR=$BASEDIR/jpeg
 cd tiff-${TIFFVERSION}
   sh -c "LDFLAGS=\"-L$BASEDIR/lib $LDSTATICOPTION\" \
     CFLAGS=\"-O2\" ./configure \
-    --with-jpeg-include-dir=$JPEGDIR \
-    --with-jpeg-lib-dir=$JPEGDIR/.libs --prefix=$BASEDIR"
+    --with-jpeg-include-dir=$BASEDIR/include \
+    --with-jpeg-lib-dir=$BASEDIR/lib --prefix=$BASEDIR \
+    --enable-shared=no"
   cd port ; make ; cd ..
   cd libtiff ; make ; cd ..
   cd tools
   make > make.out
   rm -f cmds-remake-static
   grep '^libtool:' make.out | \
-    sed -e "s/^libtool: link://;s|$JPEGDIR/\.libs/libjpeg\.[a-z][a-z.]*||" \
+    sed -e "s/^libtool: link://;s|$BASEDIR/lib/libjpeg\.[a-z][a-z.]*||" \
     > cmds-remake-static
   cat cmds-remake-static
   sh cmds-remake-static
